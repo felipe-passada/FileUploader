ac022c4fe5d11b6aa7d451c58b9488b800a39631
felipepassada <felipepassada@gmail.com>
2019-04-19 13:31:51 -0300

    uploaded file on mongodb

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
- 
-routes.post("/posts", multer(multerConfig).single('file'), (req, res) => {
-    console.log(req.file);
 
-    return res.json({hello: "Hello World!"});
+const Post = require("./models/Post");
+ 
+routes.post("/posts", multer(multerConfig).single('file'), async(req, res) => {
+    const { originalname: name, size, filename: key } = req.file;
+    
+    const post = await Post.create({
+        name: req.file.originalname,
+        size: req.file.size,
+        key: req.file.filename,
+        url: ''
+    }); 
+
+    return res.json(post);
 });
 
 module.exports = routes
